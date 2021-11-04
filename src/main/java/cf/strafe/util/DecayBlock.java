f1ce2403c3f6a83903b8bc29b7b32ba909a4aaf8
SomeRandomDev <41921648+ChangDaddy@users.noreply.github.com>
2021-11-04 17:27:50 -0400

    Using AtomicInts instead of arrays lol

--- src/main/java/cf/strafe/util/DecayBlock.java
+++ src/main/java/cf/strafe/util/DecayBlock.java
@@ -11,30 +11,31 @@ import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class DecayBlock {
 
     public DecayBlock(Location location, BlockItem blockItem) {
         final int breakId = new Random().nextInt(2000);
-        final int[] stage = {0};
-        final int[] progress = {-1};
-        final int[] count = {0};
-        final int[] id = {0};
-        id[0] = Bukkit.getScheduler().scheduleSyncRepeatingTask(KnockBackFFA.INSTANCE.getPlugin(), () -> {
-            if(count[0]++ > 2) {
-                stage[0]++;
-                count[0] = 0;
+        final AtomicInteger stage = new AtomicInteger(0);
+        final AtomicInteger progress = new AtomicInteger(-1);
+        final AtomicInteger count = new AtomicInteger(0);
+        final AtomicInteger id = new AtomicInteger(0);
+        id.set(Bukkit.getScheduler().scheduleSyncRepeatingTask(KnockBackFFA.INSTANCE.getPlugin(), () -> {
+            if(count.getAndIncrement() > 2) {
+                stage.getAndIncrement();
+                count.set(0);
             }
-            if(stage[0] > 3) {
-                progress[0] = 0;
+            if(stage.get() > 3) {
+                progress.set(0);
             }
-            if(progress[0] >= 0) {
+            if(progress.get() >= 0) {
                 for (Player player : Bukkit.getOnlinePlayers()) {
-                    PacketPlayOutBlockBreakAnimation packetPlayOutBlockBreakAnimation = new PacketPlayOutBlockBreakAnimation(breakId, new BlockPosition(location.getX(), location.getY(), location.getZ()), progress[0]);
+                    PacketPlayOutBlockBreakAnimation packetPlayOutBlockBreakAnimation = new PacketPlayOutBlockBreakAnimation(breakId, new BlockPosition(location.getX(), location.getY(), location.getZ()), progress.get());
                     ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packetPlayOutBlockBreakAnimation);
                 }
             }
-            switch (stage[0]) {
+            switch (stage.get()) {
                 case 1:
                     location.getBlock().setTypeIdAndData(blockItem.getPhase1().getData().getItemTypeId(), blockItem.getPhase1().getData().getData(), false);
                     break;
@@ -43,10 +44,10 @@ public class DecayBlock {
                     break;
                 case 3:
                     location.getBlock().setType(Material.AIR);
-                    Bukkit.getScheduler().cancelTask(id[0]);
+                    Bukkit.getScheduler().cancelTask(id.get());
                     break;
             }
-            progress[0]++;
-        }, 0, 10);
+            progress.getAndIncrement();
+        }, 0, 10));
     }
 }
